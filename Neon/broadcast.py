efdfe1a36b10cb9fa39c7f67a9c2608de585fe74
NeonAn <justasneon@gmail.com>
2025-10-02 01:29:17 +0530

    Update broadcast.py

--- Neon/broadcast.py
+++ Neon/broadcast.py
@@ -1,3 +1,4 @@
+#Broadcast.py
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
 from database.db import db
 from pyrogram import Client, filters
@@ -31,9 +32,9 @@ async def verupikkals(bot, message):
     users = await db.get_all_users()
     b_msg = message.reply_to_message
     if not b_msg:
-        return await message.reply_text("**Reply This Command To Your Broadcast Message**")
+        return await message.reply_text("**__Reply This Command To Your Broadcast Message__**")
     sts = await message.reply_text(
-        text='Broadcasting your messages...'
+        text='**__Broadcasting Your Messages...__**'
     )
     start_time = time.time()
     total_users = await db.total_users_count()
@@ -57,13 +58,18 @@ async def verupikkals(bot, message):
                     failed += 1
             done += 1
             if not done % 20:
-                await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
+                await sts.edit(f"**__Broadcast In Progress:\n\nTotal Users:** {total_users}\n**Completed:** {done} / {total_users}\n**Success:** {success}\n**Blocked:** {blocked}\n**Deleted:** {deleted}__")    
         else:
             # Handle the case where 'id' key is missing in the user dictionary
             done += 1
             failed += 1
             if not done % 20:
-                await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
+                await sts.edit(f"**__Broadcast in progress:\n\nTotal Users:** {total_users}\n**Completed:** {done} / {total_users}\n**Success:** {success}\n**Blocked:** {blocked}\n**Deleted:** {deleted}__")    
     
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+    await sts.edit(f"**__Broadcast Completed:\nCompleted in** {time_taken} **seconds.\n\nTotal Users** {total_users}\n**Completed:** {done} / {total_users}\n**Success:** {success}\n**Blocked:** {blocked}\n**Deleted:** {deleted}__")
+
+
+# Dont remove Credits
+# Developer Telegram @MyselfNeon
+# Update channel - @NeonFiles
