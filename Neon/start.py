424fae2b01de4af47b3185c7be4de6c56aa5825f
NeonAn <justasneon@gmail.com>
2025-10-14 22:37:53 +0530

    Update start.py

--- Neon/start.py
+++ Neon/start.py
@@ -102,7 +102,8 @@ async def send_start(client: Client, message: Message):
 async def send_help(client: Client, message: Message):
     await client.send_message(
         chat_id=message.chat.id,
-        text=f"{HELP_TXT}"
+        text=f"{HELP_TXT}",
+        quote=True  # this will reply to the /help command message
     )
 
 # -------------------
