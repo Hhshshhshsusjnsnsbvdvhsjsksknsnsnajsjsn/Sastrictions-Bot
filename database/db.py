10f661a7acc5f8146fec3d8189a5bc112a2efedf
NeonAn <justasneon@gmail.com>
2025-10-22 18:36:03 +0530

    Add files via upload

--- database/db.py
+++ database/db.py
@@ -1,3 +1,15 @@
+# ---------------------------------------------------
+# File Name: DB.py
+# Author: NeonAnurag
+# GitHub: https://github.com/MyselfNeon/
+# Telegram: https://t.me/MyelfNeon
+# YouTube: https://youtube.com/@MyselfNeon
+# Created: 2025-10-21
+# Last Modified: 2025-10-22
+# Version: Latest
+# License: MIT License
+# ---------------------------------------------------
+
 import motor.motor_asyncio
 from config import DB_NAME, DB_URI
 
@@ -8,22 +20,19 @@ class Database:
         self.db = self._client[database_name]
         self.col = self.db.users
 
-    # --- Updated to include username ---
-    def new_user(self, id, name, username="N/A"):
+    def new_user(self, id, name):
         return dict(
-            id=id,
-            name=name,
-            username=username,   # store username
-            session=None
+            id = id,
+            name = name,
+            session = None,
         )
     
-    # --- Updated add_user to accept username ---
-    async def add_user(self, id, name, username="N/A"):
-        user = self.new_user(id, name, username)
+    async def add_user(self, id, name):
+        user = self.new_user(id, name)
         await self.col.insert_one(user)
     
     async def is_user_exist(self, id):
-        user = await self.col.find_one({'id': int(id)})
+        user = await self.col.find_one({'id':int(id)})
         return bool(user)
     
     async def total_users_count(self):
@@ -44,3 +53,8 @@ class Database:
         return user.get('session')
 
 db = Database(DB_URI, DB_NAME)
+
+
+# MyselfNeon
+# Don't Remove Credit 🥺
+# Telegram Channel @NeonFiles
