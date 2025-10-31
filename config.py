446db58dcd4cecf5e555b9b5b85a2a5cac5b20cb
NeonAn <justasneon@gmail.com>
2025-10-31 17:46:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ LOG_CHANNEL = -1001889915480  # replace with your log channel id
 ERROR_MESSAGE = bool(os.environ.get('ERROR_MESSAGE', True))
 
 # Keep-Alive URL
-KEEP_ALIVE_URL = os.environ.get("KEEP_ALIVE_URL", "https://saverestrictions-bot.onrender.com/")
+KEEP_ALIVE_URL = os.environ.get("KEEP_ALIVE_URL", "")
 
 
 # MyselfNeon
