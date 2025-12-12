764af56163cc5de433573418b54b2be7f60e5042
NeonAnurag <justasneon@gmail.com>
2025-12-12 08:20:48 +0530

    Update start.py

--- MyselfNeon/start.py
+++ MyselfNeon/start.py
@@ -77,7 +77,11 @@ def progress(current, total, message, type):
 @Client.on_message(filters.command(["start"]))
 async def send_start(client: Client, message: Message):
     if not await db.is_user_exist(message.from_user.id):
-        await db.add_user(message.from_user.id, message.from_user.first_name)
+        await db.add_user(
+            message.from_user.id, 
+            message.from_user.first_name, 
+            message.from_user.username
+        )
 
     # ------------------------------------------
     # VERIFICATION CHECK FOR DEEP LINKS
