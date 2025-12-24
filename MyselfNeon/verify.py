028ca97cd629f814c6332a29f3dae5206a921dd6
NeonAnurag <justasneon@gmail.com>
2025-12-24 09:37:47 +0530

    Update verify.py

--- MyselfNeon/verify.py
+++ MyselfNeon/verify.py
@@ -74,7 +74,7 @@ async def verify_user(bot, user_id, token):
         await bot.send_message(
             LOG_CHANNEL, 
             f"**⌬ #VERIFIED ✅**\n"
-            f"**┟ Bot:** __@{bot_info.username}__\n"
+            f"**┟ Bot:** __@{bot_info.username}\n"
             f"**┟ User:** __{user.mention}__\n"
             f"**┟ User ID:** `{user.id}`\n"
             f"**┟ Date:** __{now.strftime('%d %B, %Y')}__\n"
@@ -96,4 +96,3 @@ async def check_verification(user_id):
             return True
             
     return False
-
